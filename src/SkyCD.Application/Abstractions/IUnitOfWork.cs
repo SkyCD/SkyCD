72a1bdc29ea339672cb0c4b54f437e9e2d38c625
Raimondas Rimkevičius <github@mekdrop.name>
2026-05-09 02:13:33 +0300

    [306] Remove Application project and catalog VM remnants

--- src/SkyCD.Application/Abstractions/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace SkyCD.Application.Abstractions;
-
-public interface IUnitOfWork
-{
-    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
-}
--- src/SkyCD.Presentation/ViewModels/Catalog/CatalogEntryType.cs
+++
@@ -1,9 +0,0 @@
-namespace SkyCD.Presentation.ViewModels.Catalog;
-
-public enum CatalogEntryType
-{
-    File = 0,
-    Media = 1,
-    Folder = 2,
-    NetworkResource = 3
-}
