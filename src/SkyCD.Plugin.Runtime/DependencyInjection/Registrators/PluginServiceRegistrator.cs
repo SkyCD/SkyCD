698c8c97481661ee41e37e56c35f58e7bbf057a5
Raimondas Rimkevičius <github@mekdrop.name>
2026-05-11 02:13:27 +0300

    [306] Simplify plugin registrator service wiring

--- src/SkyCD.Plugin.Runtime/DependencyInjection/Registrators/PluginServiceRegistrator.cs
+++ src/SkyCD.Plugin.Runtime/DependencyInjection/Registrators/PluginServiceRegistrator.cs
@@ -2,11 +2,8 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using DryIoc;
-using SkyCD.Couchbase;
-using SkyCD.Couchbase.Repository;
 using SkyCD.Plugin.Abstractions.Capabilities;
 using SkyCD.Plugin.Runtime.Discovery;
-using SkyCD.Plugin.Runtime.Documents;
 using SkyCD.Plugin.Runtime.Factories;
 using SkyCD.Plugin.Runtime.Managers;
 
@@ -14,23 +11,11 @@ namespace SkyCD.Plugin.Runtime.DependencyInjection.Registrators;
 
 public sealed class PluginServiceRegistrator : IServiceRegistrator
 {
-    private static IRegistrator AddPluginService(IRegistrator registrator, Type serviceType, object serviceInstance)
-    {
-        registrator.RegisterInstance(serviceType, serviceInstance, ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
-        registrator.RegisterInstance(serviceType, serviceInstance, serviceKey: serviceType, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
-
-        return registrator;
-    }
-
     public void RegisterServices(IRegistrator registrator)
     {
         registrator.Register<AssembliesListFactory>(Reuse.Singleton, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
-        registrator.Register<DiscoveredPluginFactory>(Reuse.Singleton, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
-        registrator.RegisterDelegate<IRepository<PluginDocument>>(static resolver =>
-        {
-            var repositoryManager = resolver.Resolve<RepositoryManager>();
-            return (IRepository<PluginDocument>)repositoryManager.For<PluginDocument>();
-        }, Reuse.Singleton, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
+        registrator.Register<DiscoveredPluginFactory>(Reuse.Singleton,
+            ifAlreadyRegistered: IfAlreadyRegistered.Replace);
         registrator.Register<PluginManager>(Reuse.Singleton, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
     }
 
@@ -52,7 +37,8 @@ public sealed class PluginServiceRegistrator : IServiceRegistrator
 
     public static void RegisterServices(IRegistrator registrator, IReadOnlyList<DiscoveredPlugin> plugins)
     {
-        registrator.RegisterInstance<IReadOnlyCollection<DiscoveredPlugin>>(plugins, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
+        registrator.RegisterInstance<IReadOnlyCollection<DiscoveredPlugin>>(plugins,
+            ifAlreadyRegistered: IfAlreadyRegistered.Replace);
         registrator.RegisterInstance<IReadOnlyDictionary<string, DiscoveredPlugin>>(
             plugins.ToDictionary(static plugin => plugin.Id, StringComparer.OrdinalIgnoreCase),
             ifAlreadyRegistered: IfAlreadyRegistered.Replace);
@@ -70,8 +56,16 @@ public sealed class PluginServiceRegistrator : IServiceRegistrator
         ArgumentNullException.ThrowIfNull(runtimeServiceProvider);
         ArgumentNullException.ThrowIfNull(plugins);
 
-        return runtimeServiceProvider.CreateSubcontainer(
-            registrator => RegisterServices(registrator, plugins));
+        return runtimeServiceProvider.CreateSubcontainer(registrator => RegisterServices(registrator, plugins));
     }
 
+    private static IRegistrator AddPluginService(IRegistrator registrator, Type serviceType, object serviceInstance)
+    {
+        registrator.RegisterInstance(serviceType, serviceInstance,
+            ifAlreadyRegistered: IfAlreadyRegistered.AppendNewImplementation);
+        registrator.RegisterInstance(serviceType, serviceInstance, serviceKey: serviceType,
+            ifAlreadyRegistered: IfAlreadyRegistered.Replace);
+
+        return registrator;
+    }
 }
